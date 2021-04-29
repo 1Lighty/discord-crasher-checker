2545f96c59b7a6b7200e0e8faccde6c9a3773aad
1Lighty <49841131+1Lighty@users.noreply.github.com>
2021-04-29 10:48:32 +0200

    Safeguard DCCWASM against FS errors

--- DCCWASMInterface.worker.js
+++ DCCWASMInterface.worker.js
@@ -1,17 +1,26 @@
 onmessage = ({ data: [file, id] }) => {
-    if (!FS.analyzePath('/work').exists) FS.mkdir('/work');
-    FS.mount(WORKERFS, { files: [file] }, '/work');
-    const frames = Module.isSafe('/work/' + file.name);
+    const path = `/work-${id}`;
+    try {
+        if (!FS.analyzePath(path).exists) FS.mkdir(path);
+        FS.mount(WORKERFS, { files: [file] }, path);
+        const frames = Module.isSafe(`${path}/${file.name}`);
 
-    data = {
-        ret: frames,
-        isSafe: frames === 0,
-        id
-    };
-    postMessage(data);
-
-    // Cleanup mount.
-    FS.unmount('/work');
+        postMessage({
+            ret: frames,
+            isSafe: frames === 0,
+            id
+        });
+    } catch (err) {
+        postMessage({
+            ret: -4,
+            isSafe: false,
+            id
+        });
+    } finally {
+        try {
+            FS.unmount(path);
+        } catch { };
+    }
 }
 
 self.importScripts('https://1lighty.github.io/discord-crasher-checker/DCCWASM.js');
