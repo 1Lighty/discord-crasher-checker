f81e781a397b2fc95eeb7a516eb0c65672183d8f
1Lighty <49841131+1Lighty@users.noreply.github.com>
2021-05-01 13:55:01 +0200

    patch GIF picker items to stop crashers

--- index.jsx
+++ index.jsx
@@ -9,7 +9,7 @@
 const { Plugin } = require('powercord/entities');
 const { findInReactTree } = require('powercord/util');
 const { inject, uninject } = require('powercord/injector');
-const { React, getModuleByDisplayName } = require('powercord/webpack');
+const { React, getModuleByDisplayName, getModule } = require('powercord/webpack');
 const { Button, Flex, Text, Tooltip } = require('powercord/components');
 
 const { Checking, DecodeError, Unsafe } = require('./Icons');
@@ -72,6 +72,7 @@ module.exports = class DiscordCrasherChecker extends Plugin {
     this.worker.start();
     suppressErrors(this.patchMediaPlayer.bind(this))();
     suppressErrors(this.patchLazyImage.bind(this))();
+    suppressErrors(this.patchGIFPickerSearchItem.bind(this))();
     this.loadStylesheet('style.css');
   }
 
@@ -245,10 +246,77 @@ module.exports = class DiscordCrasherChecker extends Plugin {
     });
   }
 
+  patchGIFPickerSearchItem() {
+    const { GIFPickerSearchItem } = getModule(['GIFPickerSearchItem'], false);
+    const _this = this;
+    const ensureProtocol = url => {
+      if (url.indexOf('http')) return `https:${url}`;
+      return url;
+    };
+    inject('discord-crasher-checker-gif-picker-search-item-mount', GIFPickerSearchItem.prototype, 'componentDidMount', function(_, ret) {
+      if (this.props.format !== 'VIDEO' || !this._video) return ret;
+      const cachedVal = _this.getCached(ensureProtocol(this.props.src));
+      this.state.__DCC_isSafe = cachedVal;
+      if (cachedVal) return ret;
+      const { _video } = this;
+      _video.autoplay = false;
+      this.setState({ __DCC_isChecking: true });
+      _this.worker.checkVideo(ensureProtocol(this.props.src)).then(isSafe => {
+        _this.setCached(ensureProtocol(this.props.src), isSafe);
+        if (!_video) return; // bruh
+        if (isSafe) {
+          _video.autoplay = true;
+          if (this._video) _video.play();
+        }
+        this.setState({ __DCC_checked: true, __DCC_isChecking: false, __DCC_isSafe: isSafe });
+      }).catch(err => {
+        Logger.error(err);
+        this.setState({ __DCC_checked: true, __DCC_isChecking: false, __DCC_error: true });
+      });
+      return ret;
+    });
+    inject('discord-crasher-checker-gif-picker-search-item', GIFPickerSearchItem.prototype, 'render', function(_, ret) {
+      if (this.props.format !== 'VIDEO' || _this.getCached(ensureProtocol(this.props.src)) || this.state.__DCC_isSafe) return ret;
+      if (!findInReactTree(ret, e => e && typeof e.className === 'string' && !e.className.indexOf('favButton'))) return ret;
+      ret.props.children = (
+        <Flex align={Flex.Align.CENTER} justify={Flex.Justify.CENTER} direction={Flex.Direction.VERTICAL} className='dcc-backdrop' key='DCC-Checker'>
+          <Text size={Text.Sizes.SIZE_20}>{
+            this.state.__DCC_isChecking ? (
+              <Tooltip text='Please wait, checking'>
+                <Checking size={48}/>
+              </Tooltip>) :
+              this.state.__DCC_error ? (
+                <Tooltip text='Decoder error'>
+                  <DecodeError size={48}/>
+                </Tooltip>) :
+                (
+                  <Tooltip text='This will crash you'>
+                    <Unsafe size={48}/>
+                  </Tooltip>)
+          }</Text>
+          { !this.state.__DCC_error && !this.state.__DCC_isChecking ? (
+            <Button look={Button.Looks.OUTLINED} onClick={e => {
+              _this.setCached(ensureProtocol(this.props.src), true);
+              this._video.autoplay = true;
+              this._video.play();
+              this.setState({ __DCC_isSafe: true });
+              e.preventDefault();
+              e.stopPropagation();
+              return false;
+            }}>Override</Button>
+          ) : null}
+        </Flex>
+      );
+      return ret;
+    });
+  }
+
   pluginWillUnload() {
     this.worker.stop();
     uninject('discord-crasher-checker-media-player-pre');
     uninject('discord-crasher-checker-media-player');
     uninject('discord-crasher-checker-lazy-image');
+    uninject('discord-crasher-checker-gif-picker-search-item-mount');
+    uninject('discord-crasher-checker-gif-picker-search-item');
   }
 };
